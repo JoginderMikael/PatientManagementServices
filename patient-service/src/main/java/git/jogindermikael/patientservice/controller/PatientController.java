7ba89362aebd0f83a891c11b98f5b2641eabfd76
Joginder Mikael <jogindermikael@gmail.com>
2026-08-11 22:19:08 +0300

    created the patient controller

---
+++ patient-service/src/main/java/git/jogindermikael/patientservice/controller/PatientController.java
@@ -0,0 +1,29 @@
+package git.jogindermikael.patientservice.controller;
+
+import git.jogindermikael.patientservice.dto.PatientResponseDTO;
+import git.jogindermikael.patientservice.model.Patient;
+import git.jogindermikael.patientservice.service.PatientService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/patients")
+public class PatientController {
+
+    private PatientService patientService;
+
+
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<PatientResponseDTO>> getPatients() {
+        return ResponseEntity.ok().body(patientService.getPatients());
+    }
+
+}
