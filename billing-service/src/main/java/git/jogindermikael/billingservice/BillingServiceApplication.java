4024d2f1dc65ed796aef956097a60df1f343aaf2
Joginder Mikael <jogindermikael@gmail.com>
2026-08-14 23:54:34 +0300

    added billing service

---
+++ billing-service/src/main/java/git/jogindermikael/billingservice/BillingServiceApplication.java
@@ -0,0 +1,13 @@
+package git.jogindermikael.billingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BillingServiceApplication.class, args);
+    }
+
+}
---
+++ billing-service/src/test/java/git/jogindermikael/billingservice/BillingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package git.jogindermikael.billingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
