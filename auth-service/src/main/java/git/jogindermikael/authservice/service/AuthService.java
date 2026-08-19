4b5ab630a462bcdd620a8db495974f1698cf665e
Joginder Mikael <jogindermikael@gmail.com>
2026-08-19 15:22:19 +0300

    token and pass encoder implementation.

--- auth-service/src/main/java/git/jogindermikael/authservice/service/AuthService.java
+++ auth-service/src/main/java/git/jogindermikael/authservice/service/AuthService.java
@@ -1,15 +1,30 @@
 package git.jogindermikael.authservice.service;
 
 import git.jogindermikael.authservice.dto.LoginRequestDto;
+import git.jogindermikael.authservice.util.JwtUtil;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+
 @Service
 public class AuthService {
+
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+
+    public AuthService(UserService userService, PasswordEncoder passwordEncoder, JwtUtil jwtUtil) {
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
+    }
+
     public Optional<String> authenticate(LoginRequestDto loginRequestDto) {
 
-
-        return Optional.empty();
+        return userService.findByEmail(loginRequestDto.getEmail())
+                .filter(u -> passwordEncoder.matches(loginRequestDto.getPassword(), u.getPassword()))
+                .map(u -> jwtUtil.generateToken(u.getEmail(), u.getRole()));
     }
 }
