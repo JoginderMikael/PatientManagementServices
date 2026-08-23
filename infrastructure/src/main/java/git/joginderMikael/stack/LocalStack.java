e64b2d224f29fc4767dd575fdb8907040d4073fb
Joginder Mikael <jogindermikael@gmail.com>
2026-08-23 20:12:00 +0300

    Create vpc and database instance

--- infrastructure/src/main/java/git/joginderMikael/stack/LocalStack.java
+++ infrastructure/src/main/java/git/joginderMikael/stack/LocalStack.java
@@ -1,7 +1,14 @@
 package git.joginderMikael.stack;
 import software.amazon.awscdk.*;
+import software.amazon.awscdk.services.ec2.Vpc;
+import software.amazon.awscdk.services.rds.DatabaseInstance;
+import software.amazon.awscdk.services.rds.DatabaseInstanceEngine;
+import software.amazon.awscdk.services.rds.PostgresEngineVersion;
+import software.amazon.awscdk.services.rds.PostgresInstanceEngineProps;
 
 public class LocalStack extends Stack {
+    private final Vpc vpc;
+
 
     public LocalStack(
             final App scope,
@@ -9,8 +16,25 @@ public class LocalStack extends Stack {
             final StackProps props) {
 
         super(scope, id, props);
+
+        this.vpc = createVpc();
     }
 
+    private Vpc createVpc() {
+       return Vpc.Builder.create(this, "PatientManagementVPC")
+               .vpcName("PatientManagementVPC")
+                .maxAzs(2)
+                .build();
+    }
+
+    private DatabaseInstance createDatabase(String id, String dbName) {
+        return DatabaseInstance.Builder
+                .create(this, id)
+                .engine(DatabaseInstanceEngine.postgres(PostgresInstanceEngineProps.builder()
+                                .version(PostgresEngineVersion.VER_17_2)
+                        .build()))
+                .build();
+    }
     public static void main(final String[] args) {
 
         System.out.println("Starting synthesis...");
