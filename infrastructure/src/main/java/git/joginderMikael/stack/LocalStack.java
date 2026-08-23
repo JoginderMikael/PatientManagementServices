8c7cb3b3abcd607f10a2e21ce1d07cdb7e63499d
Joginder Mikael <jogindermikael@gmail.com>
2026-08-23 19:45:58 +0300

    addressed errors

--- infrastructure/src/main/java/git/joginderMikael/stack/LocalStack.java
+++ infrastructure/src/main/java/git/joginderMikael/stack/LocalStack.java
@@ -2,17 +2,35 @@ package git.joginderMikael.stack;
 import software.amazon.awscdk.*;
 
 public class LocalStack extends Stack {
-    public LocalStack(final App scope, final String id, final StackProps props){
+
+    public LocalStack(
+            final App scope,
+            final String id,
+            final StackProps props) {
+
         super(scope, id, props);
     }
 
     public static void main(final String[] args) {
-        App app = new App(AppProps.builder().outdir("./cdk.out").build());
+
+        System.out.println("Starting synthesis...");
+
+        App app = new App(
+                AppProps.builder()
+                        .outdir("./infrastructure/cdk.out")
+                        .build()
+        );
+
         StackProps props = StackProps.builder()
                 .synthesizer(new BootstraplessSynthesizer())
                 .build();
+
         new LocalStack(app, "localstack", props);
+
         app.synth();
-        System.out.println("App synthesizing in progress...");
+
+        System.out.println(
+                "App synthesis complete. Check the 'cdk.out' folder for output files."
+        );
     }
 }
